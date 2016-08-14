716584625059904d59742b184570f3993b1578ff
fitzr <fitzroy.t.hagi@gmail.com>
2016-08-14 11:48:18 +0900

    Fix bug of check argments

--- src/main/main.go
+++ src/main/main.go
@@ -8,21 +8,21 @@ import (
 )
 
 func main() {
-    check(len(os.Args) < 3, "required arguments : command input_file_path output_db_source ")
+    if len(os.Args) < 3 {
+        log.Fatal("required arguments : command input_file_path output_db_source")
+    }
 
     fp, err := os.Open(os.Args[1])
-    check(err != nil, "file cannot open" + err.Error())
+    if err != nil {
+        log.Fatal("file cannot open : ", err)
+    }
     defer fp.Close()
 
     conn, err := db.Open(os.Args[2])
-    check(err != nil, "db cannot open : " + err.Error())
+    if err != nil {
+        log.Fatal("db cannot open : ", err)
+    }
     defer conn.Close()
 
     counter.Count(fp, conn)
-}
-
-func check(check bool, msg string) {
-    if check {
-        log.Fatal(msg)
-    }
 }
