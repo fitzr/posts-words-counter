c7aafa90a22ea471fdfc9d6153c4705ff63905e3
fitzr <fitzroy.t.hagi@gmail.com>
2016-08-31 13:32:53 +0900

    Mod circle.yml for test report.

--- db/connection.go
+++ db/connection.go
@@ -3,7 +3,6 @@ package db
 import (
 	"database/sql"
 	"github.com/fitzr/posts-words-counter/writer"
-	"log"
 )
 
 // DB connection.
@@ -20,7 +19,6 @@ type conn struct {
 func Open(driverName, dataSourceName string) (DB, error) {
 	db, err := sql.Open(driverName, dataSourceName)
 	if err != nil {
-		log.Print("hooooooo")
 		return nil, err
 	}
 	err = db.Ping()
